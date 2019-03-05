9b02a6d1cd9e33f17eeb5408f311d47f3baede53
Riccardo <riccardo_london@live.it>
2019-03-05 00:47:29 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
 def unsafe?(speed)
+	if speed > 60 || speed < 40
+		return true
+	else
+		return false
+	end
+
 
 end
 
 
 
 def not_safe?(speed)
-	
+	if speed > 60 || speed < 40
+		return true
+	else
+		return 50
+	end
+
 end
-	
-
-
